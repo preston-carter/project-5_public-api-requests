a4771441b4b135203adee7bc1d263549237bc58d
Preston Carter <preston.alex.carter@gmail.com>
2019-11-05 11:29:41 -0600

    Update scripts.js
    Added modal section with prev/next buttons + click events for buttons and options to close the modal view.

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ const $body = $('body');
 let profileData = [];
 
 /*
-* Fetch API Functions
+* Fetch API Function
 */
 
 fetch('https://randomuser.me/api/?results=12&nat=us&inc=picture,name,email,location,cell,dob')
@@ -24,6 +24,7 @@ fetch('https://randomuser.me/api/?results=12&nat=us&inc=picture,name,email,locat
   .then(response => response.json())
   .then(data => {
     profileData = data.results;
+    console.log(profileData);
     appendProfileData(profileData);
   })
   .catch(error => console.log('Looks like there was a problem!', error));
@@ -60,8 +61,7 @@ function appendProfileData(profile) {
 
     $profileGallery.append($profileCard);
 
-    $profileCard.on('click', () => {
-      console.log(profile);
+    $profileCard.click( () => {
       addModalWindow(profile);
     });
   });
@@ -69,9 +69,14 @@ function appendProfileData(profile) {
 
 function addModalWindow(profile) {
 
-  const $selectedProfModal = $('<div class="modal-container"></div>');
+  const $modalContainer = $('<div class="modal-container"></div>');
 
-  $selectedProfModal.html(`
+  const cell = profile.cell;
+  const cellConstructor = `${cell.substr(0,5)} ${cell.substr(6,)}`;
+  const bday = profile.dob.date;
+  const bdayConstructor = `${bday.substr(5,2)}/${bday.substr(8,2)}/${bday.substr(0,4)}`;
+
+  $modalContainer.html(`
     <div class="modal">
       <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
       <div class="modal-info-container">
@@ -80,14 +85,60 @@ function addModalWindow(profile) {
           <p class="modal-text">${profile.email}</p>
           <p class="modal-text cap">${profile.location.city}</p>
           <hr>
-          <p class="modal-text">${profile.cell}</p>
-          <p class="modal-text">${profile.location.street}, ${profile.location.city}, ${profile.location.state} ${profile.location.postcode}</p>
-          <p class="modal-text">Birthday: ${profile.location.date}</p>
+          <p class="modal-text">${cellConstructor}</p>
+          <p class="modal-text">${profile.location.street.number} ${profile.location.street.name},
+            ${profile.location.city}, ${profile.location.state} ${profile.location.postcode}</p>
+          <p class="modal-text">Birthday: ${bdayConstructor}</p>
       </div>
     </div>
   `);
 
-  $body.append($selectedProfModal);
-  console.log($selectedProfModal);
+  $body.append($modalContainer);
 
+  let profileIndex = $.inArray(profile, profileData);
+  const $modalButtons = $('<div class="modal-btn-container"></div>');
+
+  if( profileIndex === 0) {
+    $modalButtons.html(`<button type="button" id="modal-next" class="modal-next btn">Next</button>
+    `);
+    $('.modal').append($modalButtons);
+  }
+  else if ( profileIndex === 11) {
+    $modalButtons.html(`<button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+    `);
+    $('.modal').append($modalButtons);
+  }
+  else {
+    $modalButtons.html(`<button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+      <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    `);
+    $('.modal').append($modalButtons);
+  }
+
+  $('.modal-close-btn').click( () => {
+    $modalContainer.remove();
+  });
+
+  $body.click( (e) => {
+    if($modalContainer.show()) {
+      if( $(e.target).hasClass('modal-container')) {
+        $modalContainer.remove();
+      }
+    }
+  });
+
+  const $prevButton = $('#modal-prev');
+  const $nextButton = $('#modal-next');
+
+  $prevButton.click( () => {
+    $modalContainer.remove();
+    profileIndex -= 1;
+    addModalWindow(profileData[profileIndex]);
+  });
+
+  $nextButton.click( () => {
+    $modalContainer.remove();
+    profileIndex += 1;
+    addModalWindow(profileData[profileIndex]);
+  });
 }
